fbc0df346de4b536040c2164a8b72ae49e388387
HarsshaVardhan <2100030187cseh@gmail.com>
2025-03-25 21:56:29 +0530

    Fixing PDF paths

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,12 +5,14 @@ function Resume() {
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>Resume</h1>
       <iframe 
-        src={process.env.PUBLIC_URL + "/Images/resume.pdf"} 
+        src={process.env.PUBLIC_URL + "/resume.pdf"} 
         width="80%" 
         height="600px"
         style={{ border: "none" }}
         title="Resume"
       ></iframe>
+
+
     </div>
   );
 }
