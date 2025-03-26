cbe7e2b8cbc2307acde14cf119a8071e9f65ef50
HarsshaVardhan <2100030187cseh@gmail.com>
2025-03-26 20:22:20 +0530

    Fixed Resume PDF display

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,12 +5,13 @@ function Resume() {
     <div style={{ textAlign: "center", padding: "50px" }}>
       <h1>Resume</h1>
       <iframe 
-        src={process.env.PUBLIC_URL + "/resume.pdf"} 
+        src={`${process.env.PUBLIC_URL}/resume.pdf`} 
         width="80%" 
         height="600px"
         style={{ border: "none" }}
         title="Resume"
       ></iframe>
+
     </div>
   );
 }
